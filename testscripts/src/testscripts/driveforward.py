dc8706994beb09c73f4283996796f01def4e65bb
Janik Mayr <32466915+jan222ik@users.noreply.github.com>
2021-09-26 21:09:45 +0200

    Simple controller: Drive Robot Forward (#2)
    * Simple script to drive the robot forwards
    
    * Fixed model already exists
    
    * Robot drives forward
    
    * Added script to launch driveforwards example

---
+++ testscripts/src/testscripts/driveforward.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Twist
+
+
+class Forward:
+    def __init__(self):
+        rospy.init_node('driveforward')
+        self.rate = rospy.Rate(20)
+        self.botVelPub = rospy.Publisher('cmd_vel', Twist, queue_size=1)
+
+    def step(self):
+        twist = Twist()
+        twist.linear.x = 0.3
+        self.botVelPub.publish(twist)
+
+
+def main():
+    controller = Forward()
+    print("Forward node")
+    try:
+        while not rospy.is_shutdown():
+            controller.step()
+    except rospy.ROSInterruptException:
+        pass
+
+
+if __name__ == '__main__':
+    main()
