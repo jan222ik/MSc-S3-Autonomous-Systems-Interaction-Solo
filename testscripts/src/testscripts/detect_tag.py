19fe494ee0068fb8b70bfd6f26acb406be0e5ccd
Matthias Rupp <mru3383@students.fhv.at>
2021-11-17 22:42:22 +0100

    Added script for tag detections and first try, not working currently

---
+++ testscripts/src/testscripts/detect_tag.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import roslib
+# roslib.load_manifest('my_package')
+import sys
+import rospy
+import cv2
+from std_msgs.msg import String
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
+
+class image_converter:
+
+  def __init__(self):
+    self.image_pub = rospy.Publisher("/rupp/image_topic_color",Image)
+    # self.image_grey_pub = rospy.Publisher("/rupp/image_topic_grey",Image)
+    self.bridge = CvBridge()
+    self.image_sub = rospy.Subscriber("/camera/rgb/image_raw",Image,self.callback)
+
+
+  def callback(self,data):
+    try:
+      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+      print("IN CALLBACK")
+
+    except CvBridgeError as e:
+      print(e)
+
+    # cv2.imshow("TEST", cv_image)
+    hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
+
+    # create a binary thresholded image on hue between red and yellow
+    lower = (0, 240, 160)
+    upper = (30, 255, 255)
+    thresh = cv2.inRange(hsv, lower, upper)
+
+    # apply morphology
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9, 9))
+    clean = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (15, 15))
+    clean = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+
+    # get external contours
+    contours = cv2.findContours(clean, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    print(len(contours))
+    contours = contours[0] if len(contours) == 2 else contours[1]
+
+    result1 = cv_image.copy()
+    result2 = cv_image.copy()
+    cv2.imshow("TEST", cv_image)
+    for c in contours:
+      cv2.drawContours(result1, [c], 0, (0, 0, 0), 2)
+      # get rotated rectangle from contour
+      rot_rect = cv2.minAreaRect(c)
+      box = cv2.boxPoints(rot_rect)
+      box = np.int0(box)
+      # draw rotated rectangle on copy of img
+      cv2.drawContours(result2, [box], 0, (0, 0, 0), 2)
+
+    # save result
+    # cv2.imwrite("4cubes_thresh.jpg", thresh)
+    # cv2.imwrite("4cubes_clean.jpg", clean)
+    # cv2.imwrite("4cubes_result1.png", result1)
+    # cv2.imwrite("4cubes_result2.png", result2)
+
+    # display result
+    try:
+      #self.image_pub.publish(self.bridge.cv2_to_imgmsg(thresh, "bgr8"))
+      #self.image_pub.publish(self.bridge.cv2_to_imgmsg(clean, "bgr8"))
+      self.image_pub.publish(self.bridge.cv2_to_imgmsg(result1, "bgr8"))
+      #self.image_pub.publish(self.bridge.cv2_to_imgmsg(result2, "bgr8"))
+      # self.image_grey_pub.publish(self.bridge.cv2_to_imgmsg(cv_image_grey, "mono8"))
+    except CvBridgeError as e:
+      print(e)
+    #cv2.imshow("thresh", thresh)
+    #cv2.imshow("clean", clean)
+    #cv2.imshow("result1", result1)
+    #cv2.imshow("result2", result2)
+    cv2.waitKey(3)
+
+    try:
+      self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+    except CvBridgeError as e:
+      print(e)
+
+def main(args):
+  rospy.init_node('image_converter', anonymous=True)
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print("Shutting down")
+  cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)
