f8ef77f8ff74617e66b6d54d2cb2e1f010336413
Matthias Rupp <mru3383@students.fhv.at>
2021-12-09 01:09:09 +0100

    Added test for SLAM & first nodes for transformation

--- testscripts/src/testscripts/detect_tag.py
+++ testscripts/src/testscripts/detect_tag.py
@@ -7,8 +7,9 @@ import sys
 import rospy
 import cv2
 from std_msgs.msg import String
-from sensor_msgs.msg import Image
+from sensor_msgs.msg import Image, CameraInfo
 from cv_bridge import CvBridge, CvBridgeError
+from image_geometry.cameramodels import PinholeCameraModel
 import numpy as np
 
 class tag_detector:
@@ -18,11 +19,20 @@ class tag_detector:
     # self.image_grey_pub = rospy.Publisher("/rupp/image_topic_grey",Image)
     self.bridge = CvBridge()
     self.image_sub = rospy.Subscriber("/camera/rgb/image_raw",Image,self.callback)
+    #self.camera_param_desc = rospy.Subscriber("/camera/parameter_descriptions", self.param_callback)
+    self.camera_rgb_info = rospy.Subscriber("/camera/rgb/camera_info", CameraInfo, self.cam_callback)
+    self.cam = PinholeCameraModel()
+    self.not_cam_setup = True
 
+  def cam_callback(self, data):
+    if self.not_cam_setup:
+      self.cam.fromCameraInfo(data)
+      self.not_cam_setup = False
 
   def callback(self,data):
     try:
       cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+
 
     except CvBridgeError as e:
       print(e)
@@ -50,8 +60,13 @@ class tag_detector:
     #cv2.imshow("TEST", cv_image)
     for c in contours:
       cv2.drawContours(result1, [c], 0, (0, 0, 0), 2)
-      #print(c)
-      #print(c[0])
+      print(c[:][0][0])
+      x = c[0][0][0]
+      y = c[0][0][1]
+      cv2.circle(cv_image, (x, y), 20, (0, 255, 0))
+      rect_point = self.cam.rectifyPoint((x, y))
+      cam_point = self.cam.projectPixelTo3dRay(rect_point)
+      print((x, y), ", with image_geometry: ", cam_point)
       # get rotated rectangle from contour
       rot_rect = cv2.minAreaRect(c)
       box = cv2.boxPoints(rot_rect)
@@ -65,8 +80,7 @@ class tag_detector:
     # cv2.imwrite("4cubes_clean.jpg", clean)
     # cv2.imwrite("4cubes_result1.png", result1)
     # cv2.imwrite("4cubes_result2.png", result2)
-
-    #cv2.imshow("image", cv_image)
+    cv2.imshow("image", cv_image)
     #cv2.imshow("thresh", thresh)
     #cv2.imshow("clean", clean)
     cv2.imshow("result1", result1)
@@ -74,7 +88,7 @@ class tag_detector:
     cv2.waitKey(3)
 
     try:
-      #self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
+      self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
       #self.image_pub.publish(self.bridge.cv2_to_imgmsg(thresh, "8UC1"))
       #self.image_pub.publish(self.bridge.cv2_to_imgmsg(clean, "8UC1"))
       self.image_pub.publish(self.bridge.cv2_to_imgmsg(result1, "8UC3"))
