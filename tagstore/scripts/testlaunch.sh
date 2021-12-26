152c9ec2edd87978398e6f475d927aad8384f017
jan <32466915+jan222ik@users.noreply.github.com>
2021-12-26 19:57:37 +0100

    Added example for service call

--- tagstore/scripts/testlaunch.sh
+++ tagstore/scripts/testlaunch.sh
@@ -9,6 +9,11 @@ roslaunch turtlebot3_slam turtlebot3_slam.launch slam_methods:=gmapping
 # Start Tagstore
 roslaunch tagstore tagstore.launch
 
+# Reset RVis MarkerArray for tags
+rosservice call /tagstore-reset-rvis-markers
+
 
 # Call service
-rosservice call /tagstore-addtag 12 17
+rosservice call /tagstore-addtag -- 150 150
+
+
