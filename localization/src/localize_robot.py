46cb3620b764178423a93e2fe1a32561d735c657
Matthias Rupp <mru3383@students.fhv.at>
2022-01-22 13:13:27 +0100

    Added deconstruction of PoseWithCovarianceStamped, edit launch file

--- localization/src/localize_robot.py
+++ localization/src/localize_robot.py
@@ -6,16 +6,48 @@ import rospy
 import sys
 from geometry_msgs.msg import PoseWithCovarianceStamped
 from nav_msgs.msg import OccupancyGrid, MapMetaData
+import numpy as np
 
 
 class Localizer:
     def __init__(self):
         self.pose_estimate = rospy.Subscriber("/amcl_pose", PoseWithCovarianceStamped, self.pose_estimate_callback)
-        self.occupancy_grid = rospy.wait_for_message("/map", OccupancyGrid)
+        # self.occupancy_grid = rospy.wait_for_message("/map", OccupancyGrid)
         pass
 
     def pose_estimate_callback(self, estimated_pose):
-        rospy.loginfo(estimated_pose)
+        # Deconstructing PoseWithCovarianceStamped
+        ## Header
+        header = estimated_pose.header
+        seq = header.seq
+        time_stamp = header.stamp
+        frame_id = header.frame_id
+
+        ## Pose with covariance
+        pose_with_cov = estimated_pose.pose
+
+        ### Pose
+        pose = pose_with_cov.pose
+
+        #### Point
+        point = pose.position
+        point_x = point.x
+        point_y = point.y
+        point_z = point.z
+
+        #### Quaternion
+        quaternion = pose.orientation
+        quat_x = quaternion.x
+        quat_y = quaternion.y
+        quat_z = quaternion.z
+        quat_w = quaternion.w
+
+        ### Covariance (float64[36])
+        covariance = pose_with_cov.covariance
+        cov_arr = np.asarray(covariance)
+        cov_mat = np.reshape(cov_arr, (6, 6))
+
+        rospy.loginfo(cov_mat)
 
 def main(args):
     loc = Localizer()
