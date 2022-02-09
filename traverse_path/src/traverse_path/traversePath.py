a880cf4e7136f959e91aa76f1a6ee4acc193f6cb
jan222ik <jan.222.ik@gmail.com>
2022-02-09 01:14:05 +0100

    Added experimental path waypoint thinning

--- traverse_path/src/traverse_path/traversePath.py
+++ traverse_path/src/traverse_path/traversePath.py
@@ -20,33 +20,68 @@ class TraversePath:
         rospy.loginfo("TraversePath: Startup")
         self.rate = rospy.Rate(200)
         self.pubRvisMarkerArray = rospy.Publisher("traversal_points_marker_array", MarkerArray, queue_size = 1)
-        rospy.logdebug("TraversePath: Await map message for MapInfo")
+        rospy.loginfo("TraversePath: Await map message for MapInfo")
         self.mapInfo = MapMetaData()
         self.mapInfo = rospy.wait_for_message("map", OccupancyGrid).info
-        rospy.logdebug("TraversePath: Waiting for Plodding Action Server")
+        rospy.loginfo("TraversePath: Waiting for Plodding Action Server")
         self.ploddingClient = actionlib.SimpleActionClient("plodding_action_server", PlodAction)
         self.ploddingClient.wait_for_server()
-        rospy.logdebug("TraversePath: Received Plodding Action Server")
-        rospy.logdebug("TraversePath: Starting Action Server")
+        rospy.loginfo("TraversePath: Received Plodding Action Server")
+        rospy.loginfo("TraversePath: Starting Action Server")
         self.actionServer = actionlib.SimpleActionServer("traversal_action_server", TraversePathAction, execute_cb=self._nextActionGoal, auto_start = False)
         self.actionServer.start()
-        rospy.logdebug("TraversePath: Started Action Server")
+        rospy.loginfo("TraversePath: Started Action Server")
 
     def thinTraversalPoints(self, points):
         isV = False
         isH = False
         last = points[0]
-        n = []
+        n = [last]
+        countEmpty = 0
 
-        for idx in range(1, len(points)):
+        for idx in range(1, len(points) - 1):
             it = points[idx]
+            rospy.loginfo("Traverse > Thin Points: Idx:{} isH:{} isV{} dx:{} dy:{}".format(idx, isH, isV, abs(last.x - it.x), abs(last.y - it.y)))
             dx = abs(last.x - it.x) == 1
             dy = abs(last.y - it.y) == 1
+
+            add = False
+            if dx and dy:
+                if isH and isV:
+                    pass
+                else:
+                    add = True
+            else:
+                if isV:
+                    if dy:
+                        pass
+                    else:
+                        add = True
+                elif isH:
+                    if dx:
+                        pass
+                    else:
+                        add = True
+
+            if add or countEmpty > 3:
+                n.append(it)
+                countEmpty = 0
+            else:
+                countEmpty += 1
+
+            isH = dx
+            isV = dy
+            last = it
+
+        n.append(points[len(points) - 1])
+        rospy.loginfo("Traverse > Thin Points: Before:{} After:{}".format(len(points), len(n)))
+        return n
+
 
 
 
     def _nextActionGoal(self, data):
-        self.traversalPoints = data.traversal_points
+        self.traversalPoints = self.thinTraversalPoints(data.traversal_points)
         self.externalCancel = False
         self.traversalIdx = 0
 
