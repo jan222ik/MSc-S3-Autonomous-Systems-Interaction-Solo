b5af16facca701941efad01c2cddf3ad17eac643
Matthias Rupp <mru3383@students.fhv.at>
2022-02-09 19:14:06 +0100

    Added tag detection to occupancy grid

--- camera/src/camera/camera_V2.py
+++ camera/src/camera/camera_V2.py
@@ -15,7 +15,7 @@ from tf.transformations import euler_from_quaternion
 from transformations_odom.msg import PoseInMap, PoseTF
 from nav_msgs.msg import Odometry, OccupancyGrid
 
-RATE = 4
+RATE = 0
 # HSV (Hue, Saturation, V)
 # H : (0, 10), S: (200, 255), V: (20, 255)
 #lab
@@ -55,6 +55,8 @@ class TagsToMap:
         self.map_camera = [[]]
         self.map_camera_seen_seq = 0
         self.map_sub = rospy.Subscriber('map', OccupancyGrid, self._handle_map_update)
+        self.last_img_pose = None
+        self.is_calculating_image = False
 
         print("Setup done")
 
@@ -260,7 +262,6 @@ class TagsToMap:
         y = int(y)
         offset_x = int(offset_x)
         offset_y = int(offset_y)
-        print("SETTING MAP SEEN...")
         y_mask = 0
         x_mask = 0
 
@@ -279,12 +280,15 @@ class TagsToMap:
             for x1 in range(x_start, x_end, x_increment):
                 # check that not outside of map
                 if y1 >= 0 and x1 >= 0 and y1 < len(self.map_camera) and x1 < len(self.map_camera[0]):
-                    if mask[y_mask][x_mask] == 1:
+                    if mask[y_mask][x_mask] == 1 and not self.map_camera[y1][x1] > 1:
                         self.map_camera[y1][x1] = 1
+                        if self.last_img_pose is not None:
+                            (ly, lx, val) = self.last_img_pose
+                            if y1 == ly and x1 == lx:
+                                self.map_camera[y1][x1] = val
                 x_mask = x_mask + 1
 
             y_mask = y_mask + 1
-        print("DONE UPDATING MAP")
 
     def _update_map_camera_seen(self, x, y, yaw):
         """
@@ -292,7 +296,6 @@ class TagsToMap:
         """
 
         yaw = (yaw + math.pi / 2.0 + 2 * math.pi) % (2 * math.pi)
-        print("MADE IT TO YAW")
 
         # North
         if (yaw >= (15.0 / 16.0 * 2.0 * math.pi) and yaw < (2 * math.pi)) or (
@@ -335,7 +338,6 @@ class TagsToMap:
             self._map_camera_set_seen(x, y, self.mask_camera_seen_north_west, self.mask_camera_seen_north_west_offset_x,
                                       self.mask_camera_seen_north_west_offset_y)
 
-        print("MADE IT TO PUBLISH")
 
         self.map_camera_seen_seq = self.map_camera_seen_seq + 1
 
@@ -373,7 +375,6 @@ class TagsToMap:
         self.do_detection(cv_image)
 
     def img_raw_callback(self, data):
-        print("ROWS: ", data.width, ", COLS: ", data.height)
         cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
         self.do_detection(cv_image)
 
@@ -391,114 +392,115 @@ class TagsToMap:
         return qy, qx
 
     def do_detection(self, cv_image):
-        print("DETECTION_COUNT: ", self.rate_count)
-        rate = rospy.get_param('/cam_rate', 4)
-        if self.rate_count >= rate:
-            print("DOING DETECTION!!!")
-            hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
+        if not self.is_calculating_image:
+            self.is_calculating_image = True
+            rate = rospy.get_param('/cam_rate', 4)
+            if self.rate_count >= rate:
+                print("DOING DETECTION!!!")
+                hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
 
-            # create a binary thresholded image on hue
-            lower_hsv = (rospy.get_param('lower_h', 0),
-                         rospy.get_param('lower_s', 150),
-                         rospy.get_param('lower_v', 150))
-            upper_hsv = (rospy.get_param('upper_h', 40),
-                         rospy.get_param('upper_s', 255),
-                         rospy.get_param('upper_v', 255))
+                # create a binary thresholded image on hue
+                lower_hsv = (rospy.get_param('lower_h', 0),
+                             rospy.get_param('lower_s', 150),
+                             rospy.get_param('lower_v', 150))
+                upper_hsv = (rospy.get_param('upper_h', 40),
+                             rospy.get_param('upper_s', 255),
+                             rospy.get_param('upper_v', 255))
 
-            lower = lower_hsv
-            upper = upper_hsv
-            thresh = cv2.inRange(hsv, lower, upper)
+                lower = lower_hsv
+                upper = upper_hsv
+                thresh = cv2.inRange(hsv, lower, upper)
 
-            # apply morphology
-            kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9, 9))
-            clean = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
-            kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (15, 15))
-            clean = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
+                # apply morphology
+                kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (9, 9))
+                clean = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel)
+                kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (15, 15))
+                clean = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
 
-            # get external contours
-            contours = cv2.findContours(clean, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            contours = contours[0] if len(contours) == 2 else contours[1]
+                # get external contours
+                contours = cv2.findContours(clean, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                contours = contours[0] if len(contours) == 2 else contours[1]
 
-            center_arr = []
-            (h, w) = cv_image.shape[:2]  # w:image-width and h:image-height
-            center_x = w//2
-            center_y = h//2
-            # loop over contours
-            if len(contours) == 0:
-                return
+                center_arr = []
+                (h, w) = cv_image.shape[:2]  # w:image-width and h:image-height
+                center_x = w//2
+                center_y = h//2
+                # loop over contours
+                if len(contours) == 0:
+                    return
+                else:
+                    for c in contours:
+                        # compute the center of the contour
+                        M = cv2.moments(c)
+                        if M["m00"] != 0.0:
+                            cX = int(M["m10"] / M["m00"])
+                            cY = int(M["m01"] / M["m00"])
+
+                            # draw the contour and center of the shape on the image
+                            cv2.drawContours(cv_image, [c], -1, (0, 255, 0), 2)
+                            cv2.circle(cv_image, (cX, cY), 7, (255, 255, 255), -1)
+                            cv2.putText(cv_image, "center", (cX - 20, cY - 20),
+                                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                            cv2.circle(cv_image, (center_x, h), 7, (0, 0, 255), -1)
+                            cv2.putText(cv_image, "center_image", (center_x - 20, h - 20),
+                                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+                            cv2.line(cv_image, (center_x, h), (cX, cY), (255, 0, 0), 2)
+
+                            # TODO: remove for real sim runs
+                            center_x = cX  # test for simulation, since camera is not aligned properly there
+
+                            # origin is in camera center, move other point
+                            new_origin = (h - h, center_x - center_x)
+                            new_cen = (cY - h, cX - center_x)
+
+                            # flip y values
+                            flipped_origin = (new_origin[0] * -1, new_origin[1])
+                            flipped_cen = (new_cen[0] * -1, new_cen[1])
+
+                            # calculate vector
+                            vec = (flipped_cen[0] - flipped_origin[0], flipped_cen[1] - flipped_origin[1])
+
+                            # calculate line length
+                            new_line_len = np.linalg.norm((vec[0], vec[1]))
+                            # calulcate angle between origin and center
+                            new_line_angle = (math.atan2(vec[0], vec[1]) * 180) / math.pi
+                            #new_line_angle = int((math.atan2((flipped_origin[1] - flipped_cen[1]), (flipped_origin[0] - flipped_cen[0])) * 180 / math.pi))
+                            if new_line_angle < 0:
+                                # new_line_angle = 360 + new_line_angle
+                                print("HOW DID THIS HAPPEN?!")
+
+                            if self.robot_pose is not None:
+                                # get robot angel and rotate
+                                rot_point = self.rotate(flipped_origin, flipped_cen, self.robot_pose.angle)
+                                # calculate map pos
+                                # TODO: determine proper scaling
+                                map_y = self.robot_pose.y + rot_point[0]//20
+                                map_x = self.robot_pose.x + (-1 * rot_point[1]//20)  # x direction is flipped in map
+                                map_x = int(map_x)
+                                map_y = int(map_y)
+                                area = self._calc_cov_area(self.turtle_covar)
+                                val = -1
+                                if area <= 0.00001:
+                                    val = 100
+                                elif area <= 0.0001:
+                                    val = 80
+                                elif area <= 0.001:
+                                    val = 60
+                                elif area <= 0.1:
+                                    val = 50
+                                self.last_img_pose = (map_x, map_y, val)
+                                #self._publish_map_camera()
+
+                                #print("TAG POS IN MAP: Y: ", map_y, ", X: ", map_x)
+                                # TODO: works in principle, but uncertainty leads to tag overload, fix this
+                                #self.add_tag_with_tagstore(int(map_y), int(map_x))
+
+                cv2.imshow("image", cv_image)
+                self.rate_count = 0
+                cv2.waitKey(3)
+                self.is_calculating_image = False
             else:
-                for c in contours:
-                    # compute the center of the contour
-                    M = cv2.moments(c)
-                    if M["m00"] != 0.0:
-                        cX = int(M["m10"] / M["m00"])
-                        cY = int(M["m01"] / M["m00"])
-
-                        # draw the contour and center of the shape on the image
-                        cv2.drawContours(cv_image, [c], -1, (0, 255, 0), 2)
-                        cv2.circle(cv_image, (cX, cY), 7, (255, 255, 255), -1)
-                        cv2.putText(cv_image, "center", (cX - 20, cY - 20),
-                                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                        cv2.circle(cv_image, (center_x, h), 7, (0, 0, 255), -1)
-                        cv2.putText(cv_image, "center_image", (center_x - 20, h - 20),
-                                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-                        cv2.line(cv_image, (center_x, h), (cX, cY), (255, 0, 0), 2)
-
-                        # TODO: remove for real sim runs
-                        center_x = cX  # test for simulation, since camera is not aligned properly there
-
-                        # origin is in camera center, move other point
-                        new_origin = (h - h, center_x - center_x)
-                        new_cen = (cY - h, cX - center_x)
-
-                        # flip y values
-                        flipped_origin = (new_origin[0] * -1, new_origin[1])
-                        flipped_cen = (new_cen[0] * -1, new_cen[1])
-
-                        # calculate vector
-                        vec = (flipped_cen[0] - flipped_origin[0], flipped_cen[1] - flipped_origin[1])
-
-                        # calculate line length
-                        new_line_len = np.linalg.norm((vec[0], vec[1]))
-                        # calulcate angle between origin and center
-                        new_line_angle = (math.atan2(vec[0], vec[1]) * 180) / math.pi
-                        #new_line_angle = int((math.atan2((flipped_origin[1] - flipped_cen[1]), (flipped_origin[0] - flipped_cen[0])) * 180 / math.pi))
-                        if new_line_angle < 0:
-                            # new_line_angle = 360 + new_line_angle
-                            print("HOW DID THIS HAPPEN?!")
-                        print("NEW LINE LENGTH: ", new_line_len)
-                        print("NEW LINE ANGLE: ", new_line_angle)
-
-                        if self.robot_pose is not None:
-                            # get robot angel and rotate
-                            rot_point = self.rotate(flipped_origin, flipped_cen, self.robot_pose.angle)
-                            # calculate map pos
-                            # TODO: determine proper scaling
-                            map_y = self.robot_pose.y + rot_point[0]//20
-                            map_x = self.robot_pose.x + (-1 * rot_point[1]//20)  # x direction is flipped in map
-                            map_x = int(map_x)
-                            map_y = int(map_y)
-                            area = self._calc_cov_area(self.turtle_covar)
-                            print("AREA: ", area)
-                            if area <= 0.3:
-                                self.map_camera[map_x][map_y] = 100
-                            elif area <= 0.6:
-                                self.map_camera[map_x][map_y] = 80
-                            elif area <= 1.0:
-                                self.map_camera[map_x][map_y] = 60
-
-                            self._publish_map_camera()
-
-                            print("TAG POS IN MAP: Y: ", map_y, ", X: ", map_x)
-                            # TODO: works in principle, but uncertainty leads to tag overload, fix this
-                            #self.add_tag_with_tagstore(int(map_y), int(map_x))
-
-            cv2.imshow("image", cv_image)
-            self.rate_count = 0
-            cv2.waitKey(3)
-            # self.isActive = False
-        else:
-            self.rate_count += 1
+                self.rate_count += 1
 
 
 def main(args):
